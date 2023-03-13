4f74afa67ee19980ea1e8589e9fd3e91447fd293
felixfabri <felix.fabrijr@gmail.com>
2023-03-13 11:59:51 +0000

    Add project files.

---
+++ Listas_NFibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Listas_NFibonacci
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+            var numeroFibo = new List<int> { 1, 1 };
+
+            while (numeroFibo.Count <=30)
+            {
+
+
+                var numeroAnterior1 = numeroFibo[numeroFibo.Count - 1];
+                var numeroAnterior2 = numeroFibo[numeroFibo.Count - 2];
+                var item = numeroAnterior1 + numeroAnterior2;
+                numeroFibo.Add(item);
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Processo Finalizado.");
+        }
+    }
+}
